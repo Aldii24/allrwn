f736ee548518fbca80436268013fd66007561444
Aldii24 <aldiirawan748@gmail.com>
2024-04-27 16:09:53 +0700

    add-url

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const menu = document.getElementById("hamburger-menu")
 const navbarNav = document.querySelector(".navbar-nav")
 
+//when menu is clicked 
 menu.addEventListener("click", () => {
     navbarNav.classList.toggle("active")
 })
 
+//close menu when click outside
 document.addEventListener("click", (e) => {
     if (!navbarNav.contains(e.target) && !menu.contains(e.target)) {
         navbarNav.classList.remove("active")
@@ -17,3 +19,12 @@ const music = document.getElementById("logo")
 music.addEventListener("click", () => {
     document.getElementById("audio").play()
 })
+
+//send message
+function sendMessage() {
+    const nama = document.getElementById("name")
+    const message = document.getElementById("message")
+
+    const urlToWhatsapp = `https://wa.me/6281372648563?text=allo aldiii, namaku ${nama.value}. ${message.value}`
+    window.open(urlToWhatsapp, "_blank")
+}
