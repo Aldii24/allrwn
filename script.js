d4547969bdd330ee1043e530ad42addcd589cb1d
Aldii24 <aldiirawan748@gmail.com>
2024-04-28 09:09:32 +0700

    add-link-to

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ function sendMessage() {
     const urlToWhatsapp = `https://wa.me/6281372648563?text=allo aldiii, namaku ${nama.value}. ${message.value}`
     window.open(urlToWhatsapp, "_blank")
 }
+
+//link to instagram
+const instagram = document.getElementById("instagram")
+
+instagram.addEventListener("click", () => {
+    const urlToInstagram = "https://www.instagram.com/allrvvnn"
+    window.open(urlToInstagram, "_blank")
+})
+
+
+//link to whatsapp
+const whatsapp = document.getElementById("whatsapp")
+
+whatsapp.addEventListener("click", () => {
+    const urlToWhatsapp = "https://wa.me/6281372648563"
+    window.open(urlToWhatsapp, "_blank")
+})
+
+//link to facebook
+const facebook = document.getElementById("facebook")
+
+facebook.addEventListener("click", () => {
+    const urlToFacebook = "https://www.facebook.com/aldi.irawan.311493/"
+    window.open(urlToFacebook, "_blank")
+})
